58e7e6475f99154ed056b36c284a3d15b5ecebea
hakjongholic <i032co2p@naver.com>
2018-11-10 17:00:27 +0900

    Update runcommand-onend.sh

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -4,5 +4,5 @@
 # edit and path: $ sudo nano /opt/retropie/configs/all/runcommand-onend.sh
 
 sed '1,$d' /dev/shm/runcommand.log > /dev/shm/runcommand.log
-echo "notice" >&2
-echo "PLZ GO TO SLEEP!" >&2
+echo "captain" >&2
+echo "ALL DAY" >&2
